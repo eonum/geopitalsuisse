6246eafead63759fab0ee599235e6bfee58dc3a3
Nathalie Froidevaux <nathalie.froidevaux@students.unibe.ch>
2018-03-06 16:52:38 +0100

    integrated google maps

--- geopitalFrontend/src/app/app.component.ts
+++ geopitalFrontend/src/app/app.component.ts
@@ -2,9 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styles: [`
+    agm-map {
+      height: 700px;
+      width: 1200px;
+    }
+  `],
+  template: `
+  <agm-map [latitude]="lat" [longitude]="lng"></agm-map>
+  `
 })
 export class AppComponent {
-  title = 'app';
+  lat: number = 46.9480900;
+  lng: number = 7.4474400;
 }
--- geopitalFrontend/src/app/app.module.ts
+++ geopitalFrontend/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
+import { NgModule, ApplicationRef } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
+import { FormsModule} from "@angular/forms";
+import { CommonModule } from "@angular/common";
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    AgmCoreModule.forRoot ({
+      apiKey: 'AIzaSyBRs9r5mVR66zxTv3Jf5lG05M5jRkWf-Ws'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
