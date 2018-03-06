2956a467ff4e5b883bc56794148ee6fe9a00a115
Nathalie Froidevaux <nathalie.froidevaux@students.unibe.ch>
2018-03-06 17:56:38 +0100

    refactored google maps implementation

--- geopitalFrontend/src/app/app.component.ts
+++ geopitalFrontend/src/app/app.component.ts
@@ -2,17 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  styles: [`
-    agm-map {
-      height: 700px;
-      width: 1200px;
-    }
-  `],
-  template: `
-  <agm-map [latitude]="lat" [longitude]="lng"></agm-map>
-  `
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
 })
 export class AppComponent {
+  title: string = 'geopital suisse';
   lat: number = 46.9480900;
   lng: number = 7.4474400;
 }
