b9574a2827fc5ac7d3c543adcde476d171b28b46
Nathalie Froidevaux <nathalie.froidevaux@students.unibe.ch>
2018-03-10 15:19:34 +0100

    map style adjusted

--- geopitalFrontend/src/app/app.component.ts
+++ geopitalFrontend/src/app/app.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string = 'geopital suisse';
-  lat: number = 46.9480900;
-  lng: number = 7.4474400;
+  lat: number = 46.818188;
+  lng: number = 8.227512
 }
