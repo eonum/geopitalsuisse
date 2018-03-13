b1a8fa9cf674f4677db1ba5bd8d1b648f7bc0ac1
Angi <angela.keller@students.unibe.ch>
2018-03-13 18:25:34 +0100

    whatever

---
+++ geopitalFrontend/src/app/components/characteristics/characteristics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-characteristics',
+  templateUrl: './characteristics.component.html',
+  styleUrls: ['./characteristics.component.css']
+})
+export class CharacteristicsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
