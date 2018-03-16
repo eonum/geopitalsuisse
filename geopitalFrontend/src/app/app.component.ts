bf81937fbe9b308bc50d4b02be1331dfdd789132
Corina Masanti <cmasanti@gmx.ch>
2018-03-17 00:31:43 +0100

    trying to get data with HttpClient, does not work yet

--- geopitalFrontend/src/app/app.component.ts
+++ geopitalFrontend/src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title: string = 'geopital suisse';
   lat: number = 46.818188;
   lng: number = 8.227512
+
+  constructor(private http: HttpClient){
+
+  }
+
+  /**
+   * Gives a "connection refused" error
+   * Infos from: https://medium.com/codingthesmartway-com-blog/angular-4-3-httpclient-accessing-rest-web-services-with-angular-2305b8fd654b
+   */
+  ngOnInit(): void {
+    /*this.http.get('https://localhost:3000').subscribe(data => {
+      console.log(data);
+    });*/
+  }
 }
--- geopitalFrontend/src/app/services/characteristics.service.ts
+++ geopitalFrontend/src/app/services/characteristics.service.ts
@@ -1,17 +1,23 @@
 import Characteristics from '../models/characteristics.model';
 import { Observable } from 'rxjs/Observable';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import {Response} from '@angular/http';
 import { Injectable } from '@angular/core';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
 
 @Injectable()
 export class CharacteristicsService {
 
   api_url = 'http://localhost:3000';
   characteristicsUrl = `${this.api_url}/api/characteristics`;
+  data: any = {};
 
-  constructor(
-    private http: HttpClient
-  ) { }
+
+  /**
+   * Injects HTTP Client
+   * @param {HttpClient} http
+   */
+  constructor(private http: HttpClient) {
+
+  }
 
 }
