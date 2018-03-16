e99eee0557645735af8cd9a05fb06795651f7fb6
Manuela Eschler <manu.eschler@gmail.com>
2018-03-16 14:15:29 +0100

    Added file upload app.use

--- geopitalBackend/app.js
+++ geopitalBackend/app.js
@@ -27,6 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileUpload());
 
 app.use('/', index);
 app.use('/users', users);
