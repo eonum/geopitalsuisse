09ca83588993f85c6109a93f468c01246b9ba074
Raffael Hertle <raffaelhertle@windowslive.com>
2018-03-19 13:40:55 +0100

    add cors function

--- geopitalBackend/app.js
+++ geopitalBackend/app.js
@@ -12,8 +12,11 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 var api = require('./routes/api.route');
 var mvc = require('./routes/mvc.route');
+const cors = require('cors');
 
 var app = express();
+
+app.use(cors());
 
 // View engine setup
 app.set('views', path.join(__dirname, 'views'));
