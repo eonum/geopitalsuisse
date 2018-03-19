260ddf283df56a83e07e3e533a4748aefb76f2b4
Nathalie Froidevaux <nathalie.froidevaux@students.unibe.ch>
2018-03-19 17:24:19 +0100

    removed title

--- geopitalFrontend/src/app/app.component.ts
+++ geopitalFrontend/src/app/app.component.ts
@@ -8,7 +8,6 @@ import {HttpClient} from "@angular/common/http";
 })
 
 export class AppComponent implements OnInit{
-  title: string = 'geopital suisse';
   lat: number = 46.818188;
   lng: number = 8.227512
 
