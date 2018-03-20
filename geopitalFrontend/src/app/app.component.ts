122fb965ddc9c3d4dd6e80deb7d490779e0d0c8e
Angi <angela.keller@students.unibe.ch>
2018-03-20 14:53:49 +0100

    points on the map

--- geopitalFrontend/src/app/app.component.ts
+++ geopitalFrontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import { AgmCoreModule } from "@agm/core";
 
 @Component({
   selector: 'app-root',
