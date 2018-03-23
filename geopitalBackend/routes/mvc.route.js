4eb664ae2b43beb0d995fdb1c7d5ddcb6d52b279
Manuela Eschler <manu.eschler@gmail.com>
2018-03-23 08:57:35 +0100

    upload page from api to mvc

--- geopitalBackend/routes/mvc.route.js
+++ geopitalBackend/routes/mvc.route.js
@@ -2,9 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 var mvcController = require('../controllers/mvcController.controller')
+var fileUpload = require('./mvc/upload.route');
 
 router.get('/', mvcController.index);
 router.get('/hospitals', mvcController.hospitalList);
 router.get('/addresses', mvcController.addressList);
+router.use('/upload/', fileUpload);
 
 module.exports = router;
