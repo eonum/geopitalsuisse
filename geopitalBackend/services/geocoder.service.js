0e7e43bbe69d86ea848395c860654ceea6c769a2
UniDomi <dominic.schweizer@students.unibe.ch>
2018-03-23 09:30:11 +0100

    Implemented async function to get the coordinates of an address.

--- geopitalBackend/services/geocoder.service.js
+++ geopitalBackend/services/geocoder.service.js
@@ -10,7 +10,22 @@ var options = {
 
 var geocoder = NodeGeocoder(options);
 
-// Using callback
-geocoder.geocode('29 champs elysée paris', function(err, res) {
+exports.geocode = function(){
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(geocoder.geocode('Freiburgstrasse 8, 3010 Bern'));
+    }, 2000);
+  });
+  /*
+  return new Promise(jsonArray => {
+    geocoder.geocode('Freiburgstrasse 8, 3010 Bern', function(err, res){
+  })
+  });
+  /*
+  console.log('start');
+  geocoder.geocode('Freiburgstrasse 8, 3010 Bern', function(err, res){
   console.log(res);
+  return res;
 });
+*/
+};
--- geopitalBackend/services/geocoding.service.js
+++ geopitalBackend/services/geocoding.service.js
@@ -1,23 +1,41 @@
 var mongoose = require('mongoose');
 var Hospital = require('../models/hospital.model');
 var Address = require('../models/address.model');
+var geocoderService = require('./geocoder.service');
 
 exports.addCoordinatesToHospitals = async function(){
   Hospital.find().populate('address').exec(function (err, hospitals){
+    console.log('start');
+    getCoordinatesFromJson();
+    /*
     var list = hospitals;
     for(var i = 0; i < hospitals.length; i++){
       saveCoordinates(hospitals[i]);
     }
+    */
   });
 }
 
 saveCoordinates = async function(hospital){
+  console.log('start');
+  geocoder.geocode();
   try{
+    throw error('test');
     geocoder.geocode(hospital.address.line, function(err, res){
       console.log(res);
     })
   }
   catch(e){
     console.log('Oops');
+  }
+}
+
+getCoordinatesFromJson = async function(){
+  try{
+    console.log('this is the jsonArray');
+    var json = await geocoderService.geocode();
+    console.log(json[1].longitude);
+  }catch(e){
+    console.log(e.message);
   }
 }
