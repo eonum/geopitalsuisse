ee8f5021cd295017a05abfecf059a7bdfe993d1d
Manuela Eschler <manu.eschler@gmail.com>
2018-03-23 10:50:22 +0100

    #13 Optimize upload and displaying uploaded json

--- geopitalBackend/controllers/upload.controller.js
+++ geopitalBackend/controllers/upload.controller.js
@@ -6,12 +6,14 @@ var fs = require('fs');
 var convertExcel = require('excel-as-json').processFile;
 // Saving the context of this module inside the _the variable
 var geocodingService = require('../services/geocoding.service');
+var fs = require('fs');
 _this = this;
 
 
 // Async Controller function to get the hospital list
 exports.upload= function(req, res){
-    res.render('upload', {title: 'Excel Upload'});
+    var listJson = fs.readdirSync('../uploads/json');
+    res.render('upload', {title: 'Excel Upload', listJson: listJson});
 }
 
 exports.parse = function (req, res){
@@ -19,7 +21,7 @@ exports.parse = function (req, res){
   return("Success");
 }
 
-exports.uploadPost= function (req, res) {
+exports.uploadPost= async function (req, res) {
     //Clear uploads directory
     uploadService.uploadsDelete();
     // Was a file uploaded?
@@ -27,21 +29,34 @@ exports.uploadPost= function (req, res) {
         return res.status(400).send('No files were uploaded!');
     else{
         //saved file to uploads/ with the filename hospitalData
-        req.files.foo.mv('uploads/hospitalData.xlsx', function (err) {
+        req.files.foo.mv('../uploads/xlsx/hospitalData.xlsx', function (err) {
             if(err)
                 return res.status(400).send(err.toString());
 
 
         })
-
+        
         //convert excel to json and stores it to the uploads directory
-        convertExcel('uploads/hospitalData.xlsx', 'uploads/hospitalData.json')
+        try {
+            await convertExcel('../uploads/xlsx/hospitalData.xlsx', '../uploads/json/hospitalData.json')
+        } catch (err) {
+            console.log('Excel to Json Error: ' + err)
+            return res.status(500).send()
+        }
+        
 
-        uploadService.storeJsonImport();
+        //uploadService.storeJsonImport();
 
-        res.end('File uploaded!')
+        //wait for saving json file before reload page -- if you have a better way to do this, please tell me!!
+        try {
+            await setTimeout(function () {
+                res.redirect('/mvc/upload');
+            }, 100);
+
+        }catch(err){
+            console.log(err);
+        }
+
     }
-
-
 
 }
