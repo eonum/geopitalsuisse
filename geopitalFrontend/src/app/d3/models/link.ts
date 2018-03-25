8306eded5844a13bc0da269d2e373ce3365208c6
Angi <angela.keller@students.unibe.ch>
2018-03-25 13:21:01 +0200

    added d3 and visuals folder for further visualizations

---
+++ geopitalFrontend/src/app/d3/models/link.ts
@@ -0,0 +1,16 @@
+import { Node } from './';
+import * as d3 from 'd3';
+
+export class Link implements d3.SimulationLinkDatum<Node> {
+  // optional - defining optional implementation properties - required for relevant typing assistance
+  index?: number;
+
+  // must - defining enforced implementation properties
+  source: Node | string | number;
+  target: Node | string | number;
+
+  constructor(source, target) {
+    this.source = source;
+    this.target = target;
+  }
+}
