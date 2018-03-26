43305011f09ed3d40424c7db9e6c927b225e0c4c
UniDomi <dominic.schweizer@students.unibe.ch>
2018-03-26 15:41:28 +0200

    implemented attributes and attributeType #12 #15

--- geopitalBackend/models/hospital.model.js
+++ geopitalBackend/models/hospital.model.js
@@ -4,14 +4,15 @@ var Schema = mongoose.Schema;
 
 var Address = require('./address.model');
 var Coordinates = require('./coordinates.model');
+var Attribute = require('./attribute.model');
 
 // Define Hospital Schema
 var hostpitalSchema = Schema({
 	year: {type: Number},
   name: {type: String},
   address: {type: Schema.Types.ObjectId, ref:'Address'},
-	coordinates: {type : Schema.Types.ObjectId, ref: 'Coordinates'}
-	
+	coordinates: {type : Schema.Types.ObjectId, ref: 'Coordinates'},
+	attributes: [{type: Schema.Types.ObjectId, ref: 'Attribute'}]
 });
 
 module.exports = mongoose.model('Hospital', hostpitalSchema);
