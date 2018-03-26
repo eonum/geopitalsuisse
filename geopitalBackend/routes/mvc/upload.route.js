72dbf439eaadc49571f0342ebb128f38e4f0a50f
Manuela Eschler <manu.eschler@gmail.com>
2018-03-26 14:11:50 +0200

    #13 new route

--- geopitalBackend/routes/mvc/upload.route.js
+++ geopitalBackend/routes/mvc/upload.route.js
@@ -8,5 +8,6 @@ var uploadController = require('../../controllers/upload.controller')
 router.get('/', uploadController.upload)
 router.post('/', uploadController.uploadPost)
 router.get('/parse', uploadController.parse)
+router.post('/fileName', uploadController.fileToDBPost)
 
 module.exports = router;
