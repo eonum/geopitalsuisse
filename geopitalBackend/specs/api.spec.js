03587a0887e0be4d418e3cbc28121922ec6966da
July95 <julien.deldon@students.unibe.ch>
2018-04-03 11:44:51 +0200

    #14 added test for all dummies
    All dummies api tested.
    Not a chic testing way, still work in progress with the understanding of Jasmine-node Testing. But at least everything seems to work as it should !

--- geopitalBackend/specs/api.spec.js
+++ geopitalBackend/specs/api.spec.js
@@ -12,6 +12,7 @@ describe("api", function(){
         // Test /public/dummy hospitals
         it("returns all dummies in public", function(done){
             request.get(publicDummyUrl, function(error, response, body){
+                expect(body).toEqual('{"status":200,"data":[{"name":"Inselspital","address":{"street":"Freiburgstrasse","streetNumber":"8","plz":"3010","city":"Bern"},"coordinates":{"latitude":"46°56\'52.7N","longitude":"7°25\'29.2E"}},{"name":"Unispital Zürich","address":{"street":"Rämisstrasse","streetNumber":"100","plz":"8091","city":"Zürich"},"coordinates":{"latitude":"47°22\'36.4N","longitude":"8°33\'03.8E"}},{"name":"Unispital Basel","address":{"street":"Spitalstrasse","streetNumber":"21","plz":"4031","city":"Basel"},"coordinates":{"latitude":"47°33\'44.3N","longitude":"7°34\'59.7E"}}]}');
                 done();
             });
         });
@@ -19,6 +20,7 @@ describe("api", function(){
         // Testing dummy with id = 1
         it("returns dummy with id = 1", function(done){
             request.get(dummy1Url, function(error, response, body){
+                expect(body).toEqual('{"status":200,"data":{"name":"Unispital Zürich","address":{"street":"Rämisstrasse","streetNumber":"100","plz":"8091","city":"Zürich"},"coordinates":{"latitude":"47°22\'36.4N","longitude":"8°33\'03.8E"},"attributes":[{"name":"Anzahl Fälle","value":91238},{"name":"Anzahl Ärzte","value":351},{"name":"Personalkosten","value":4556456}]}}');
                 done();
             });
         });
@@ -26,6 +28,7 @@ describe("api", function(){
         // Testing dummy with id = 2
         it("returns dummy with id = 2", function(done){
             request.get(dummy2Url, function(error, response, body){
+                expect(body).toEqual('{"status":200,"data":{"name":"Unispital Basel","address":{"street":"Spitalstrasse","streetNumber":"21","plz":"4031","city":"Basel"},"coordinates":{"latitude":"47°33\'44.3N","longitude":"7°34\'59.7E"},"attributes":[{"name":"Anzahl Fälle","value":10123},{"name":"Anzahl Ärzte","value":100},{"name":"Personalkosten","value":1234351}]}}');
                 done();
            });
         });
@@ -33,6 +36,7 @@ describe("api", function(){
         // Testing dummy with id = 3
         it("returns dummy with id = 3", function(done){
             request.get(dummy3Url, function(error, response, body){
+                expect(body).toEqual('{"status":200}');
                 done();
             });
         });
--- geopitalBackend/specs/server.spec.js
+++ geopitalBackend/specs/server.spec.js
@@ -1,7 +1,6 @@
 // Use the request package to send a GET request
 var request = require("request");
-var baseUrl = "http://localhost:3000/mvc/";
-var server = require("../app");
+var baseUrl = "http://localhost:3000/mvc";
 
 // Gives a short description to the tested feature
 describe("geopital-backend server", function(){
@@ -15,9 +14,9 @@ describe("geopital-backend server", function(){
         });
 
         // Supposed to fail, needed for understanding
-        it("returns /mvc page", function(done) {
+        it("returns /mvc body", function(done) {
             request.get(baseUrl, function(error, response, body) {
-                expect(body).toEqual();
+                expect(body).toEqual('<!DOCTYPE html><html lang="en"><head><title>Geopital Suisse Management</title><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"><script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script><script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script><link rel="stylesheet" href="/stylesheets/style.css"></head><body><div class="container-fluid"><div class="row"><div class="col-sm-2"><ul class="sidebar-nav"><li> <a href="/mvc">Home</a></li><li> <a href="/mvc/hospitals">See hospitals</a></li><li> <a href="/mvc/create">Create hospital</a></li><li> <a href="/api/geopital">See DummyData for Frontend</a></li><li> <a href="/api/hospital">See DummyData from MongoDB</a></li><li><a href="/mvc/upload">Upload xlsx data file</a></li></ul></div><div class="col-sm-10"><h1>Geopital Suisse Management</h1><p>Welcome to Geopital Suisse Management</p><h1>Hospitals</h1><ul><li><strong>Hospitals:</strong> 2</li></ul></div></div></div></body></html>');
                 done();
             });
         });
