a496391aaa69b46432a3d67895c3f06bfb53ef95
Manuela Eschler <manu.eschler@gmail.com>
2018-04-20 11:56:07 +0200

    New route gets all data

--- geopitalBackend/controllers/hospital.controller.js
+++ geopitalBackend/controllers/hospital.controller.js
@@ -1,5 +1,6 @@
 var hospitalService = require('../services/hospital.service');
 var Hospital = require('../models/hospital.model');
+var AttributeType = require('../models/attributeType.model');
 var geopitalService = require('../services/dummy.service');
 
 var Attribute = require('../models/attribute.model');
@@ -18,6 +19,36 @@ exports.getAllHospitalsNoAttributes = async function(req, res, next){
     return res.status(400).json({status:400, message: "Here" + e.message});
   }
   //return res.status(400).json({status:400, message: 'Not yet implemented!'});
+}
+exports.getAllData = async function(req, res, next){
+    console.log("Got all");
+    try{
+        Hospital.find().populate('coordinates').populate('address').populate('attributes').populate({
+            path : 'attributes',
+            populate :{
+                path : 'attributeType',
+                model: 'AttributeType'
+            }}).exec( async function (err, hospitals){
+            var json = JSON.parse(JSON.stringify(hospitals))
+            json.forEach(function(hospital){
+                if(hospital.coordinates)
+                    delete hospital.coordinates._id;
+                if(hospital.address)
+                    delete hospital.address._id;
+                hospital.attributes.forEach(function(attribute){
+                    delete attribute._id;
+                    var nameObj = attribute.attributeType.name;
+                    var codeVar = attribute.attributeType.code;
+                    delete attribute.attributeType;
+                    attribute.name = nameObj;
+                    attribute.code = codeVar;
+                })
+            });
+            return res.status(200).json({status: 200, data: json});
+        });
+    }catch(e){
+        return res.status(400).json({status:400, message: "Here" + e.message});
+    }
 }
 exports.Dummy_getAllHospitalsNoAttributes = async function(req, res, next){
   try{
