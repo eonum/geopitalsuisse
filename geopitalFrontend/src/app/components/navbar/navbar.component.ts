ca4526742cf58346a26141cd736115c49b68ef5e
Nathalie Froidevaux <nathalie.froidevaux@students.unibe.ch>
2018-04-30 14:07:21 +0200

    deleted unused code

--- geopitalFrontend/src/app/components/navbar/navbar.component.ts
+++ geopitalFrontend/src/app/components/navbar/navbar.component.ts
@@ -23,27 +23,14 @@ export class NavbarComponent implements OnInit{
   private numSpezKl: number = 0;
 
   ngOnInit() {
-    // load all hospital data from backend
 
-    // this.hospitalService.getAll()
-    //   .subscribe(hospitals => {
-    //     // this.hospitalsList = hospitals;
-    //     // console.log("---------------------------------")
-    //     // console.log(this.hospitalsList[2].attributes[1].value);
-    //     // console.log("---------------------------------")
-    //     //mapDrawer(this.hospitalsList);
-    //   });
   }
 
   // is called when checkbox-check is changed
-  // TODO: make separate function for each call in navbar.component.html (or better version with num)
   selectHospitalType(hospitalType){
 
     if (hospitalType == 'K111') {
       this.numUniSp = this.numUniSp+1;
-      // console.log(this.numUniSp);
-      // console.log(hospitalType);
-
       this.hospitalService.getAll()
       .subscribe(hospitals => {
         this.hospitalsList = hospitals;
@@ -55,9 +42,6 @@ export class NavbarComponent implements OnInit{
 
     if (hospitalType == 'K112') {
       this.numZentSp = this.numZentSp+1;
-      // console.log(this.numZentSp);
-      // console.log(hospitalType);
-
       this.hospitalService.getAll()
       .subscribe(hospitals => {
         this.hospitalsList = hospitals;
@@ -69,9 +53,6 @@ export class NavbarComponent implements OnInit{
 
     if (hospitalType == 'K121' || hospitalType == 'K122' || hospitalType == 'K123') {
       this.numGrundVers = this.numGrundVers+1;
-      // console.log(this.numGrundVers);
-      // console.log(hospitalType);
-
       this.hospitalService.getAll()
       .subscribe(hospitals => {
         this.hospitalsList = hospitals;
@@ -83,9 +64,6 @@ export class NavbarComponent implements OnInit{
 
     if (hospitalType == 'K211' || hospitalType == 'K212') {
       this.numPsychKl = this.numPsychKl+1;
-      // console.log(this.numPsychKl);
-      // console.log(hospitalType);
-
       this.hospitalService.getAll()
       .subscribe(hospitals => {
         this.hospitalsList = hospitals;
@@ -97,9 +75,6 @@ export class NavbarComponent implements OnInit{
 
     if (hospitalType == 'K221') {
       this.numRehaKl = this.numRehaKl+1;
-      // console.log(this.numRehaKl);
-      // console.log(hospitalType);
-
       this.hospitalService.getAll()
       .subscribe(hospitals => {
         this.hospitalsList = hospitals;
@@ -112,9 +87,6 @@ export class NavbarComponent implements OnInit{
     if (hospitalType == 'K231' || hospitalType == 'K232' || hospitalType == 'K233'
     || hospitalType == 'K234' || hospitalType == 'K235') {
       this.numSpezKl = this.numSpezKl+1;
-      // console.log(this.numSpezKl);
-      // console.log(hospitalType);
-
       this.hospitalService.getAll()
       .subscribe(hospitals => {
         this.hospitalsList = hospitals;
