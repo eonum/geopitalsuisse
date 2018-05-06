089834edbf7053d313c69c50e4e90596457bfa4e
Corina Masanti <cmasanti@gmx.ch>
2018-05-06 23:03:36 +0200

    fixed dropdown id, added attribute names in dropdown

--- geopitalFrontend/src/app/components/dropdown/dropdown.component.ts
+++ geopitalFrontend/src/app/components/dropdown/dropdown.component.ts
@@ -10,11 +10,29 @@ import {Hospital} from "../../models/hospital.model";
 })
 export class DropdownComponent implements OnInit {
 
+  private hospitalsList: Hospital[];
+  private attributes: any[] = [];
+
 
   constructor(private hospitalService: HospitalService) {  }
 
   ngOnInit() {
+    this.hospitalService.getAll()
+      .subscribe(hospitals => {
+        this.hospitalsList = hospitals;
+
+        // store name of attributes (de, it, fr) in array to display in dropdown
+        // TODO: scroll in array
+        for(let i in this.hospitalsList){
+          for(let j in this.hospitalsList[i].attributes){
+            var attr = this.hospitalsList[i].attributes[j].name;
+            this.attributes.push(attr);
+          }
+        }
+      });
+
+
   }
 
- 
+
 }
