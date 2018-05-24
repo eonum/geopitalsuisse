54d9c403284590cc67353a4f9504f443d97b7b59
Nathalie Froidevaux <nathalie.froidevaux@students.unibe.ch>
2018-05-24 10:59:22 +0200

    deleted unused component

--- geopitalFrontend/src/app/models/attributes.model.ts
+++ geopitalFrontend/src/app/models/attributes.model.ts
@@ -8,25 +8,12 @@ export class Attributes {
     nameFR: String;
     nameIT: String;
 
-    // attributes: {
-    //     attributeType: {
-    //         code: String;
-    //         name: String;
-    //         group: String;
-    //       }
-    //     value: String;
-    //   }
-
     constructor(){
-        //this.attributeType = {code: "", name: {de:"", fr:"", it:""}, group: ""};
         this.code= "";
         this.category = "";
         this.nameDE="";
         this.nameFR="";
         this.nameIT="";
-        // this.address = { street: "", streetNumber: "", plz: "", city: ""};
-        // this.attributes = { attributeType: {code: "", name: "", group: ""}, value: ""};
-
     }
 
 
