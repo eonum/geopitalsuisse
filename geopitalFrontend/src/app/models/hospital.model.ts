ab17447b89c7ed112d2bf996423d66d7b1edf64a
Nathalie Froidevaux <nathalie.froidevaux@students.unibe.ch>
2018-05-30 14:24:51 +0200

    models synchronized

--- geopitalFrontend/src/app/models/hospital.model.ts
+++ geopitalFrontend/src/app/models/hospital.model.ts
@@ -3,35 +3,22 @@ export class Hospital {
   _id: String;
   name: String;
   streetAndNumber: String;
-  plzAndCity: String;
+  zipCodeAndCity: String;
   latitude: String;
   longitude: String;
 
-
-  //attributes: any[];
   hospital_attributes: {
     value: String;
     year: number;
     code: String;
   }
 
-  hospital_locations: {
-    name: String;
-    streetAndNumber: String;
-    plzAndCity: String;
-    latitude: String;
-    longitude: String;
-    la: String;
-    kanton: String;
-  }
-
-  constructor(){
-    this.name = "";
-    this.streetAndNumber = "";
-    this.plzAndCity = "";
-    this.latitude = "";
-    this.longitude = "";
-    this.hospital_attributes = { code: "", value: "", year: 0};
-    this.hospital_locations = {name: "", streetAndNumber: "", plzAndCity: "", latitude: "", longitude: "", la: "", kanton: ""}
+  constructor(name, streetAndNumber, zipCodeAndCity, latitude, longitude, attr_code, attr_value, attr_year){
+    this.name = name;
+    this.streetAndNumber = streetAndNumber;
+    this.zipCodeAndCity = zipCodeAndCity;
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.hospital_attributes = { code: attr_code, value: attr_code, year: attr_year};
   }
 }
