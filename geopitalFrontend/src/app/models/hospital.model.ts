f9572b2b6a74e03f0981cb3a5eae039a209b0242
Ramona <ramona.beck@students.unibe.ch>
2018-06-20 11:27:27 +0200

    Add default params to constructor

--- geopitalFrontend/src/app/models/hospital.model.ts
+++ geopitalFrontend/src/app/models/hospital.model.ts
@@ -13,11 +13,15 @@ export class Hospital {
   };
 
   constructor(name, streetAndNumber, zipCodeAndCity, latitude, longitude, attr_code, attr_value, attr_year){
-    this.name = name;
-    this.streetAndNumber = streetAndNumber;
-    this.zipCodeAndCity = zipCodeAndCity;
-    this.latitude = latitude;
-    this.longitude = longitude;
-    this.hospital_attributes = { code: attr_code, value: attr_value, year: attr_year};
+    this.name = name || '';
+    this.streetAndNumber = streetAndNumber || '';
+    this.zipCodeAndCity = zipCodeAndCity || '';
+    this.latitude = latitude || '';
+    this.longitude = longitude || '';
+    this.hospital_attributes = {
+      code: attr_code || '',
+      value: attr_value || 0,
+      year: attr_year || ''
+    };
   }
 }
