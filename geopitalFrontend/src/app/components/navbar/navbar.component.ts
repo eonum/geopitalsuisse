da1a43ef269437c952fccc4baedbd81ecd3103fb
Ramona <ramona.beck@students.unibe.ch>
2018-06-22 09:17:00 +0200

    Fix syntax

--- geopitalFrontend/src/app/components/navbar/navbar.component.ts
+++ geopitalFrontend/src/app/components/navbar/navbar.component.ts
@@ -14,14 +14,14 @@ declare function updateMap(numUniSp, numZentSp, numGrundVers, numPsychKl, numReh
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit{
+export class NavbarComponent implements OnInit {
 
-  private numUniSp: number = 0;
-  private numZentSp: number = 0;
-  private numGrundVers: number = 0;
-  private numPsychKl: number = 0;
-  private numRehaKl: number = 0;
-  private numSpezKl: number = 0;
+  private numUniSp = 0;
+  private numZentSp = 0;
+  private numGrundVers = 0;
+  private numPsychKl = 0;
+  private numRehaKl = 0;
+  private numSpezKl = 0;
 
 
   constructor() {
@@ -41,36 +41,36 @@ export class NavbarComponent implements OnInit{
    * Function then calls updateMap() from mapInitializer.js in which the remaining logic is implemented.
    * @param hospitalType hospitals from this type need to be displayed (if they are hidden) or hidden (if they are displayed)
    */
-  selectHospitalType(hospitalType){
+  selectHospitalType(hospitalType) {
 
-    if (hospitalType == 'K111') {
-      this.numUniSp = this.numUniSp+1;
+    if (hospitalType === 'K111') {
+      this.numUniSp = this.numUniSp + 1;
       updateMap(this.numUniSp, this.numZentSp, this.numGrundVers, this.numPsychKl, this.numRehaKl, this.numSpezKl);
       }
 
-    if (hospitalType == 'K112') {
-      this.numZentSp = this.numZentSp+1;
+    if (hospitalType === 'K112') {
+      this.numZentSp = this.numZentSp + 1;
       updateMap(this.numUniSp, this.numZentSp, this.numGrundVers, this.numPsychKl, this.numRehaKl, this.numSpezKl);
     }
 
-    if (hospitalType == 'K121' || hospitalType == 'K122' || hospitalType == 'K123') {
-      this.numGrundVers = this.numGrundVers+1;
+    if (hospitalType === 'K121' || hospitalType === 'K122' || hospitalType === 'K123') {
+      this.numGrundVers = this.numGrundVers + 1;
       updateMap(this.numUniSp, this.numZentSp, this.numGrundVers, this.numPsychKl, this.numRehaKl, this.numSpezKl);
     }
 
-    if (hospitalType == 'K211' || hospitalType == 'K212') {
-      this.numPsychKl = this.numPsychKl+1;
+    if (hospitalType === 'K211' || hospitalType === 'K212') {
+      this.numPsychKl = this.numPsychKl + 1;
       updateMap(this.numUniSp, this.numZentSp, this.numGrundVers, this.numPsychKl, this.numRehaKl, this.numSpezKl);
     }
 
-    if (hospitalType == 'K221') {
-      this.numRehaKl = this.numRehaKl+1;
+    if (hospitalType === 'K221') {
+      this.numRehaKl = this.numRehaKl + 1;
       updateMap(this.numUniSp, this.numZentSp, this.numGrundVers, this.numPsychKl, this.numRehaKl, this.numSpezKl);
     }
 
-    if (hospitalType == 'K231' || hospitalType == 'K232' || hospitalType == 'K233'
-    || hospitalType == 'K234' || hospitalType == 'K235') {
-      this.numSpezKl = this.numSpezKl+1;
+    if (hospitalType === 'K231' || hospitalType === 'K232' || hospitalType === 'K233'
+    || hospitalType === 'K234' || hospitalType === 'K235') {
+      this.numSpezKl = this.numSpezKl + 1;
       updateMap(this.numUniSp, this.numZentSp, this.numGrundVers, this.numPsychKl, this.numRehaKl, this.numSpezKl);
     }
   }
