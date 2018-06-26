fa1fc5fce9c306206439dbaaf989be19a5573ebc
Ramona <ramona.beck@bluewin.ch>
2018-06-26 14:32:57 +0200

    Add dropdown to select num attributes for scatterplot

--- geopitalFrontend/src/app/components/scatterplot/scatterplot.component.ts
+++ geopitalFrontend/src/app/components/scatterplot/scatterplot.component.ts
@@ -13,14 +13,30 @@ declare const drawGraph;
 })
 export class ScatterplotComponent implements OnInit {
 
+  public numericalAttributes: any[] = [];
   private hospitalsList: Hospital[];
-  private numericalAttributes: any;
+
   changeToView = 'Map';
+  xCoordinateNumAttribute = 'Attribut x-Achse';
+  yCoordinateNumAttribute = 'Attribut y-Achse';
 
   constructor(
     private characteristicsService: CharacteristicsService,
     private hospitalService: HospitalService
   ) { }
+
+
+  private static handleDropdownHighlight(attribute: any, id: string, axis: string): void {
+    const div = document.getElementById(id);
+    const a = div.getElementsByTagName('a');
+
+    for (let  i = 0; i < a.length; i++) {
+      if (a[i].classList.contains('active')) {
+        a[i].classList.remove('active');
+      }
+    }
+    document.getElementById(axis + '-' + attribute).classList.add('active');
+  }
 
   ngOnInit() {
     this.characteristicsService.getNumericalAttributes()
@@ -35,4 +51,37 @@ export class ScatterplotComponent implements OnInit {
       });
   }
 
+  filterNumAttr(axis: string): void {
+    const input = (axis === 'x') ? (<HTMLInputElement>document.getElementById('searchXNumAttr')) :
+      (<HTMLInputElement>document.getElementById('searchYNumAttr'));
+    const filter = input.value.toUpperCase();
+    const div = (axis === 'x') ? document.getElementById('xAxisNumAttr') : document.getElementById('yAxisNumAttr');
+    const a = div.getElementsByTagName('a');
+
+    for (let i = 0; i < a.length; i++) {
+      if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = '';
+      } else {
+        a[i].style.display = 'none';
+      }
+    }
+  }
+
+  /**
+   * Function is called when user selects an attribute in the dropdown1 from the html.
+   * @param xAxisNumAttr selected categorical attribute from dropdown1
+   */
+  selectXNumAttribute(xAxisNumAttr): void {
+    ScatterplotComponent.handleDropdownHighlight(xAxisNumAttr.nameDE, 'xAxisNumAttr', 'x');
+    this.xCoordinateNumAttribute = xAxisNumAttr.nameDE;
+  }
+
+  /**
+   * Function is called when user selects an attribute in the dropdown2 from the html.
+   * @param yAxisNumAttr selected numerical attribute from dropdown2
+   */
+  selectYNumAttribute(yAxisNumAttr): void {
+    ScatterplotComponent.handleDropdownHighlight(yAxisNumAttr.nameDE, 'yAxisNumAttr', 'y');
+    this.yCoordinateNumAttribute = yAxisNumAttr.nameDE;
+  }
 }
