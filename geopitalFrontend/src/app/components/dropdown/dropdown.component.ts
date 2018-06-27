46e6614415917bc26a3f28ace3415d1ea60e8ccc
Ramona <ramona.beck@students.unibe.ch>
2018-06-27 14:10:11 +0200

    Use d3 service, refactor component

--- geopitalFrontend/src/app/components/dropdown/dropdown.component.ts
+++ geopitalFrontend/src/app/components/dropdown/dropdown.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { CharacteristicsService } from "../../services/characteristics.service";
-
-// The declare function call is to get the D3 logic from the mapinizializer.js file
-declare function updateCircleRadius(attribute): any;
-declare function showCatOptions(attribute): any;
+import { CharacteristicsService } from '../../services/characteristics.service';
+import { D3Service } from '../../services/d3.service';
 
 /**
  * Class is responsible that the data for the attribute-options in the dropdown is correctly loaded.
@@ -22,7 +19,10 @@ export class DropdownComponent implements OnInit {
   selectedCatAttribute: string;
   selectedNumAttribute: string;
 
-  constructor(private characteristicsService: CharacteristicsService) {  }
+  constructor(
+    private characteristicsService: CharacteristicsService,
+    private d3: D3Service
+  ) {  }
 
   /**
    * Is called on init and loads the attribute-array from the backend.
@@ -35,64 +35,51 @@ export class DropdownComponent implements OnInit {
       .subscribe(attributes => {
         this.categoricalAttributes = attributes;
 
-        // extract the categorical attribute "Typ" since its not used in this selection
+        // extract the categorical attribute 'Typ' since its not used in this selection
         this.categoricalAttributes = this.categoricalAttributes.filter(attribute => {
-          return attribute.code !== "Typ";
+          return attribute.code !== 'Typ';
         });
-        this.selectedCatAttribute = this.categoricalAttributes[0].nameDE;
       });
 
     this.characteristicsService.getNumericalAttributes()
       .subscribe(attributes => {
         this.numericalAttributes = attributes;
-        this.selectedNumAttribute = this.numericalAttributes[0].nameDE;
       });
+
+    this.selectedCatAttribute = D3Service.getDefaultCategoricalAttribute().nameDE;
+    this.selectedNumAttribute = D3Service.getDefaultNumericalAttribute().nameDE;
   }
 
   /**
    * Function is called when user selects an attribute in the dropdown1 from the html.
-   * @param categorcialAttribute selected categorical attribute from dropdown1
+   * @param catAttribute selected categorical attribute from dropdown1
    */
-  selectCatAttribute(categorcialAttribute) {
-    showCatOptions(categorcialAttribute);
-    this.handleDropdownHighlight(categorcialAttribute.nameDE, "catAttr");
-    this.selectedCatAttribute = categorcialAttribute.nameDE;
+  selectCatAttribute(catAttribute: any) {
+    this.d3.updateSelectedCategoricalAttribute(catAttribute);
+    this.selectedCatAttribute = catAttribute.nameDE;
   }
 
   /**
    * Function is called when user selects an attribute in the dropdown2 from the html.
-   * @param numericalAttribute selected numerical attribute from dropdown2
+   * @param numAttribute selected numerical attribute from dropdown2
    */
-  selectNumAttribute(numericalAttribute){
-    updateCircleRadius(numericalAttribute);
-    this.handleDropdownHighlight(numericalAttribute.nameDE, "numAttr");
-    this.selectedNumAttribute = numericalAttribute.nameDE;
+  selectNumAttribute(numAttribute: any) {
+    this.d3.updateSelectedNumericalAttribute(numAttribute);
+    this.selectedNumAttribute = numAttribute.nameDE;
   }
 
   filterNumAttr() {
-    const input = (<HTMLInputElement>document.getElementById("searchNumAttr"));
+    const input = (<HTMLInputElement>document.getElementById('searchNumAttr'));
     const filter = input.value.toUpperCase();
-    const div = document.getElementById("numAttr");
-    const a = div.getElementsByTagName("a");
+    const div = document.getElementById('numAttr');
+    const a = div.getElementsByTagName('a');
 
     for (let i = 0; i < a.length; i++) {
-      if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1){
-        a[i].style.display = "";
+      if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = '';
       } else {
-        a[i].style.display = "none";
+        a[i].style.display = 'none';
       }
     }
-  }
-
-  handleDropdownHighlight(attribute: any, id: string): void {
-    const div = document.getElementById(id);
-    const a = div.getElementsByTagName("a");
-
-    for (let  i = 0; i < a.length; i++) {
-      if (a[i].classList.contains("active")) {
-        a[i].classList.remove("active");
-      }
-    }
-    document.getElementById(attribute).classList.add("active");
   }
 }
