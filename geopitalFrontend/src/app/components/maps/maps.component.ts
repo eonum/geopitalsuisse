031f1cefe60cccacc9db43ec7486cb3eb444728b
Ramona <ramona.beck@students.unibe.ch>
2018-06-28 08:29:52 +0200

    Refactor onInit method

--- geopitalFrontend/src/app/components/maps/maps.component.ts
+++ geopitalFrontend/src/app/components/maps/maps.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 import { D3Service } from '../../services/d3.service';
+import { CharacteristicsService } from '../../services/characteristics.service';
+import { HospitalService } from '../../services/hospital.service';
+import { Attribute } from '../../models/attribute.model';
+import { Hospital } from '../../models/hospital.model';
 
 
 /**
@@ -16,14 +20,36 @@ import { D3Service } from '../../services/d3.service';
 export class MapsComponent implements OnInit {
 
   changeToView = 'Scatterplot';
+  private numericalAttributes: Attribute[];
+  private categoricalAttributes: Attribute[];
+  private hospitalsList: Hospital[];
 
-  constructor ( private d3: D3Service ) {}
+  constructor (
+    private characteristicsService: CharacteristicsService,
+    private hospitalService: HospitalService,
+    private d3: D3Service
+  ) {}
 
   /**
    * Loads all hospital data from backend with the help of hospitalService
    * and gives it to the mapDrawer() function in mapInitializer.js
    */
   ngOnInit() {
-    this.d3.initializeMap();
+    this.characteristicsService.getNumericalAttributes()
+      .subscribe(x => {
+        this.numericalAttributes = x;
+
+        this.characteristicsService.getCategoricalAttributes()
+          .subscribe(y => {
+            this.categoricalAttributes = y;
+
+            this.hospitalService.getAll()
+              .subscribe(hospitals => {
+                this.hospitalsList = hospitals;
+                this.d3.drawMap(this.hospitalsList, this.numericalAttributes, this.categoricalAttributes);
+              });
+
+          });
+      });
   }
 }
