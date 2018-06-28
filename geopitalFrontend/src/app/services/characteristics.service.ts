7c4972d367d784b12c11f0b1d3b987f411a58fd6
Ramona <ramona.beck@students.unibe.ch>
2018-06-28 16:29:58 +0200

    Add methods to check if an attribute is categorical or numerical

--- geopitalFrontend/src/app/services/characteristics.service.ts
+++ geopitalFrontend/src/app/services/characteristics.service.ts
@@ -1,8 +1,9 @@
-import { Observable } from 'rxjs/Rx';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Attribute } from "../models/attribute.model";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+
+import { Attribute } from '../models/attribute.model';
 
 @Injectable()
 export class CharacteristicsService {
@@ -10,6 +11,8 @@ export class CharacteristicsService {
 
   constructor(private http: HttpClient) {}
 
+  private categoricalAttributes;
+  private numericalAttributes;
   /**
    * Gets all categorical attributes of all hospitals
    * @returns {Observable<Attributes[]>} data in form of the defined model Attributes
@@ -17,8 +20,9 @@ export class CharacteristicsService {
   getCategoricalAttributes(): Observable<Attribute[]> {
     return this.http.get<Attribute[]>('http://geopitalsuisse-backend.eonum.ch/api/attributeTypes')
       .map(res => {
+        this.categoricalAttributes = res['attribute_types_string'] as Attribute[];
         return res['attribute_types_string'] as Attribute[];
-      })
+      });
   }
 
   /**
@@ -28,7 +32,30 @@ export class CharacteristicsService {
   getNumericalAttributes(): Observable<Attribute[]> {
     return this.http.get<Attribute[]>('http://geopitalsuisse-backend.eonum.ch/api/attributeTypes')
       .map(res => {
+        this.numericalAttributes = res['attribute_types_number'] as Attribute[];
         return res['attribute_types_number'] as Attribute[];
-      })
+      });
   }
+
+  isCategoricalAttribute(attribute): boolean {
+    const position = this.categoricalAttributes.findIndex(cur => {
+      return Object.keys(attribute).every(function (key) {
+        return attribute[key] === cur[key];
+      });
+    });
+
+    return position > -1;
+
+  }
+
+  isNumericalAttribute(attribute): boolean {
+    const position = this.numericalAttributes.findIndex(cur => {
+      return Object.keys(attribute).every(function (key) {
+        return attribute[key] === cur[key];
+      });
+    });
+
+    return position > -1;
+  }
+
 }
