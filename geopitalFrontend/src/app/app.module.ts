f21191cdad07291c9775757d54a3e5f1a802016b
Ramona <ramona.beck@bluewin.ch>
2018-07-04 07:21:20 +0200

    Install ngx-translation module

--- geopitalFrontend/src/app/app.module.ts
+++ geopitalFrontend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -12,12 +12,18 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { DropdownComponent } from './components/dropdown/dropdown.component';
 import { CategorialAttributesComponent } from './components/categorial-attributes/categorial-attributes.component';
 
-import {CharacteristicsService} from './services/characteristics.service';
-import {HospitalService} from './services/hospital.service';
+import { CharacteristicsService } from './services/characteristics.service';
+import { HospitalService } from './services/hospital.service';
 import { D3Service } from './services/d3.service';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ScatterplotComponent } from './components/scatterplot/scatterplot.component';
 
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -32,6 +38,13 @@ import { ScatterplotComponent } from './components/scatterplot/scatterplot.compo
   ],
   imports: [
     BrowserModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
     HttpClientModule,
     CommonModule,
     FormsModule,
