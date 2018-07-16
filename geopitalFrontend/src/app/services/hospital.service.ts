2be3e2ece886904db093f1975836f3d449a839fe
Ramona <ramona.beck@students.unibe.ch>
2018-07-16 15:32:57 +0200

    Refactor service, get hospitals only once at the beginning because it takes too long to load them more often

--- geopitalFrontend/src/app/services/hospital.service.ts
+++ geopitalFrontend/src/app/services/hospital.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, isDevMode } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { Hospital} from '../models/hospital.model';
@@ -11,6 +11,8 @@ import { Hospital} from '../models/hospital.model';
 @Injectable()
 export class HospitalService {
 
+  private hospitals: Array<Hospital> = null;
+
   constructor(private http: HttpClient) {}
 
   /**
@@ -18,15 +20,22 @@ export class HospitalService {
    * @returns {Observable<Array<Hospital>>}
    */
   getHospitals(): Observable<Array<Hospital>> {
-    return this.http.get<Array<Hospital>>(this.getUrl() + '/api/geopital/hospitals')
-    .pipe(
-      map( res => res.map((hospital: Hospital) => new Hospital(hospital)))
-  )};
 
-
+    if (this.hospitals) {
+      return of(this.hospitals)
+    } else {
+      return this.http.get<Array<Hospital>>(HospitalService.getUrl() + '/api/geopital/hospitals')
+        .pipe(
+          map( res => {
+            this.hospitals = res.map((hospital: Hospital) => new Hospital(hospital));
+            return this.hospitals;
+          })
+        )
+    }
+  };
 
   // Todo: replace 'de' with current locale
-  private getUrl(): string {
+  private static getUrl(): string {
     if (isDevMode()) {
       return 'http://localhost:3000/de';
     } else {
