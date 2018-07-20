327f759fe134c40f774dac4a826864254bd9ac90
Ramona <ramona.beck@students.unibe.ch>
2018-07-20 12:19:57 +0200

    Delete geopitalBackend since we use qualimed hospitals as new backend

--- geopitalBackend/app/models/hospital_location.rb
+++
@@ -1,5 +0,0 @@
-class HospitalLocation < ApplicationRecord
-  validates(:name, presence: true, uniqueness: {scope: [:streetAndNumber, :zipCodeAndCity]})
-  validates(:hospital_id, presence: true)
-  belongs_to :hospital
-end
