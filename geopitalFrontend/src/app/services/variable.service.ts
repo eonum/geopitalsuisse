961e9b54534b0c993bb2979789b7c20f18cb43f5
Ramona <ramona.beck@students.unibe.ch>
2018-07-25 12:22:16 +0200

    Improve service and write some tests to prove functionality

--- geopitalFrontend/src/app/services/variable.service.ts
+++ geopitalFrontend/src/app/services/variable.service.ts
@@ -12,12 +12,18 @@ export class VariableService {
   constructor() { }
 
   static getValueOfVariable(variable: Variable): string {
-    const value = variable[Object.keys(variable)[0]].value;
-    return (value != null) ? value : null;
+    if (variable != null)  {
+      const value = variable[Object.keys(variable)[0]].value;
+      return (value != null) ? value : null;
+    }
+    return null;
   }
 
-  getVariableOfHospitalByAttribute(hospital: Hospital, attribute: Attribute) {
-    const variable: Variable = hospital.attributes.filter(obj => Object.keys(obj)[0] === attribute.code)[0];
-    return (variable != null) ? variable : null;
+  getVariableOfHospitalByAttribute(hospital: Hospital, attribute: Attribute): Variable | null {
+    if (hospital != null && attribute != null) {
+      const variable: Variable = hospital.attributes.filter(obj => Object.keys(obj)[0] === attribute.code)[0];
+      return (variable != null) ? variable : null;
+    }
+    return null;
   }
 }
