94e371396e5950d886c0cad03b30ab47e5d16f7b
Ramona <ramona.beck@students.unibe.ch>
2018-07-27 16:59:01 +0200

    Fix initial test

--- geopitalFrontend/src/app/components/menu-popover/menu-popover.component.spec.ts
+++ geopitalFrontend/src/app/components/menu-popover/menu-popover.component.spec.ts
@@ -1,23 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { HttpLoaderFactory } from '../../app.module';
 
 import { MenuPopoverComponent } from './menu-popover.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('MenuPopoverComponent', () => {
   let component: MenuPopoverComponent;
   let fixture: ComponentFixture<MenuPopoverComponent>;
+  let translate: TranslateService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MenuPopoverComponent ]
+      declarations: [
+        MenuPopoverComponent
+      ],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+          }
+        })
+      ],
+      schemas: [
+        NO_ERRORS_SCHEMA
+      ]
     })
-    .compileComponents();
+    .compileComponents().then(() => {
+      fixture = TestBed.createComponent(MenuPopoverComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+      translate = TestBed.get(TranslateService);
+    });
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MenuPopoverComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
