0b433a09d09748cc2762a8691ba064ea592d0fbc
Ramona <ramona.beck@students.unibe.ch>
2018-07-27 15:34:47 +0200

    Implement popover

--- geopitalFrontend/src/app/components/menu-popover/menu-popover.component.ts
+++ geopitalFrontend/src/app/components/menu-popover/menu-popover.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+import { faGlobe } from '@fortawesome/free-solid-svg-icons/faGlobe';
+import { faChartLine } from '@fortawesome/free-solid-svg-icons/faChartLine';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons/faEllipsisV';
+
+import { Settings } from '../../settings';
+
+declare const $: any;
 
 @Component({
   selector: 'app-menu-popover',
@@ -6,10 +16,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-popover.component.css']
 })
 export class MenuPopoverComponent implements OnInit {
+  faGlobe = faGlobe;
+  faChartLine = faChartLine;
+  faEllipsisV = faEllipsisV;
+  languages = Settings.LANGUAGES;
+  currentRoute = '';
 
-  constructor() { }
+  constructor(
+    public translate: TranslateService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
-  }
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.url.split('/').indexOf('map') > -1 ? 'map' : 'statistics';
+      }
+    });
 
+    $(function() {
+      $('[data-toggle=popover]').popover({
+        html : true,
+        content: function() {
+          const content = $(this).attr('data-popover-content');
+          return $(content).children('.popover-body').html();
+        }
+      });
+    });
+  }
 }
